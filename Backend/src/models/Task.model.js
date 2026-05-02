e9c0df0aafa1bb979359a0b064d4a2d146fd5bb8
Karmanya Pandey <karmanyapandey348@gmail.com>
2026-05-03 01:35:54 +0530

    Add Task model

---
+++ Backend/src/models/Task.model.js
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 5
+        },
+        description: {
+            type: String,
+            trim: true,
+            default: ""
+        },
+        project: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Project",
+            required: true
+        },
+        assignedTo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ["todo", "in-progress", "done"],
+            default: "todo"
+        },
+        priority: {
+            type: String,
+            enum: ["low", "medium", "high"],
+            default: "medium"
+        },
+        dueDate: {
+            type: Date
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
